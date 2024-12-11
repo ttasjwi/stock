eeb6ff5bcc78d29cc77a7d5e0afc6bd43ddbef9c
ttasjwi <ttasjwi920@gmail.com>
2024-12-11 16:48:58 +0900

    동시 100개 요청 - 실패

--- src/test/kotlin/com/example/stock/service/StockServiceTest.kt
+++ src/test/kotlin/com/example/stock/service/StockServiceTest.kt
@@ -3,15 +3,16 @@ package com.example.stock.service
 import com.example.stock.domain.Stock
 import com.example.stock.repository.StockRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
-@Transactional
 @SpringBootTest
 class StockServiceTest {
 
@@ -21,21 +22,54 @@ class StockServiceTest {
     @Autowired
     private lateinit var stockRepository: StockRepository
 
+    private var savedStockId: Long = 0L
+
     @BeforeEach
     fun setup() {
-        stockRepository.saveAndFlush(Stock.create(1L, 100))
+        savedStockId = stockRepository.saveAndFlush(Stock.create(1L, 100)).id!!
     }
 
+    @AfterEach
+    fun tearDown() {
+        stockRepository.deleteById(savedStockId)
+    }
 
     @Test
     @DisplayName("재고 수량을 1 감소시키고 조회하면 감소된 양의 Stock이 조회된다")
     fun decreaseTest() {
         // given
         // when
-        stockService.decrease(1L, 1L)
+        stockService.decrease(savedStockId, 1L)
 
         // then
-        val findStock = stockRepository.findByIdOrNull(1L)!!
+        val findStock = stockRepository.findByIdOrNull(savedStockId)!!
         assertThat(findStock.quantity).isEqualTo(99L)
+    }
+
+
+    @Test
+    @DisplayName("동시 100개 요청(실패)")
+    fun decreaseWithMultiThread() {
+        // given
+        val savedStockId = stockRepository.saveAndFlush(Stock.create(1L, 100)).id!!
+
+        val threadCount = 100
+        val executorsService = Executors.newFixedThreadPool(threadCount)
+        val countDownLatch = CountDownLatch(threadCount)
+
+        // when
+        for (i in 1..threadCount) {
+            executorsService.submit {
+                try {
+                    stockService.decrease(savedStockId, 1L)
+                } finally {
+                    countDownLatch.countDown()
+                }
+            }
+        }
+        countDownLatch.await()
+
+        val findStock = stockRepository.findByIdOrNull(savedStockId)!!
+        assertThat(findStock.quantity).isEqualTo(0L)
     }
 }
